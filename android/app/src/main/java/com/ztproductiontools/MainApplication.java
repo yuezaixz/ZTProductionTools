b26a213ef205bfbd5d6de58106968171ae8f95d6
yuezaixz <303178394@qq.com>
2018-03-08 11:18:25 +0800

    安卓配置修改

--- android/app/src/main/java/com/ztproductiontools/MainApplication.java
+++ android/app/src/main/java/com/ztproductiontools/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+import it.innove.BleManagerPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+              new MainReactPackage(),
+              new BleManagerPackage()
       );
     }
 
