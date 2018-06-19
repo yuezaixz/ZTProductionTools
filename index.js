29f721b44ff6b6d1271bb88517666ef4990cb02f
吴迪玮 <xiao303178394@gmail.com>
2018-06-19 10:15:07 +0800

    省去一些警告

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import Root from './app/root';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 YellowBox.ignoreWarnings(['Class RCTCxxModule']);
+console.ignoredYellowBox = ['Remote debugger'];
 StatusBar.setBarStyle('light-content', true);
 
 AppRegistry.registerComponent('ZTProductionTools', () => Root);
